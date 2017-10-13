a90827f0a84260a6649702371adb8ab0b0f45e6a
martabaneres <martabaneres@hotmail.com>
2017-10-13 15:48:20 +0200

    Creo adaptador y muestro la lista en la pantalla

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -2,6 +2,7 @@ package edu.upc.eseiaat.pma.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Arrays;
 
 public class CountryListActivity extends AppCompatActivity {
 
+    private ArrayAdapter<String> adapter;
     private ArrayList<String> country_list;
 
     @Override
@@ -20,5 +22,8 @@ public class CountryListActivity extends AppCompatActivity {
         country_list = new ArrayList<>(Arrays.asList(countries));
 
         ListView list = (ListView) findViewById(R.id.country_list);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, country_list);
+        list.setAdapter(adapter);
     }
 }
