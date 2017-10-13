63f7b65a2091e9c2700f31d03169bed8f93ae05c
martabaneres <martabaneres@hotmail.com>
2017-10-13 15:42:11 +0200

    La lista de países está importada a los recursos del proyecto

---
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -0,0 +1,24 @@
+package edu.upc.eseiaat.pma.countrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    private ArrayList<String> country_list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+
+        String[] countries = getResources().getStringArray(R.array.countries);
+        country_list = new ArrayList<>(Arrays.asList(countries));
+
+        ListView list = (ListView) findViewById(R.id.country_list);
+    }
+}
